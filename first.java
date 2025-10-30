06d2f00c9b6f38f49333c0a153d47b59968d92c4
hitaishipp <hitaishipandey01@gmail.com>
2025-10-30 20:11:11 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class first{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the radius of the circle");
+        float r = sc.nextFloat();
+        double area = 3.14*r*r;
+        System.out.println("the area of the circle is " + area);
+
+    }
+
+}
