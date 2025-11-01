fcd5303e3d5bcd92463426abf7628cf6ff27cc0c
hitaishipp <hitaishipandey01@gmail.com>
2025-11-01 18:32:39 +0530

    modified brute force method

--- maxSubarray.java
+++ maxSubarray.java
@@ -6,9 +6,7 @@ public class maxSubarray{      //brute force method O(n^2)
        for(int i=0;i<=arr.length-1;i++){
            int currSum = 0;
           for(int j=i;j<=arr.length-1;j++){
-              
               currSum += arr[j];
-                
               if(currSum > maxSum){
                   maxSum = currSum;
                   start=i;
@@ -24,23 +22,6 @@ public class maxSubarray{      //brute force method O(n^2)
        }
     }
 
-    // public static void maxSub2(int arr[]){
-    //      int n = arr.length;
-    //     int maxSum = Integer.MIN_VALUE;
-
-    //     for (int i = 0; i < n; i++) {
-    //         int currentSum = 0;
-
-    //         for (int j = i; j < n; j++) {
-    //             currentSum += arr[j]; 
-    //             if (currentSum > maxSum) {
-    //                 maxSum = currentSum;
-    //             }
-    //         }
-    //     }
-
-    //     return maxSum;
-    // }
     public static void main(String args[]){
         int arr[]= {2,3,-4,5,-1,2,-3,4,-2,6,-5,2};
         maxsub(arr);
