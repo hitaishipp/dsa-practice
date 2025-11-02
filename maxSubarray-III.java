e72e553d9a8133a3d0dac29c1c77d6ae56c0eeb4
hitaishipp <hitaishipandey01@gmail.com>
2025-11-02 11:14:30 +0530

    maximum Subarray using kadane's algorithm

---
+++ maxSubarray-III.java
@@ -0,0 +1,34 @@
+//kadane's algorithm
+import java.util.*;
+public class maxSubarray{
+    public static void kadane(int arr[]){
+        int maxSum= Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<arr.length;i++){
+            currSum+=arr[i];
+            if(currSum<0){
+                currSum=0;
+            }
+            maxSum=Math.max(maxSum,currSum);
+        }
+        System.out.println("maxSum is: "+maxSum);
+
+    }
+    
+     //Negative Subarray issue resolved
+        public static void kadaneModified(int arr[]){
+            int currSum=arr[0];
+            int maxSum = arr[0];
+            for(int i=1;i<arr.length;i++){
+                currSum=Math.max(arr[i],currSum+arr[i]);
+                maxSum=Math.max(maxSum,currSum);
+            }
+            System.out.println("maxSum is "+ maxSum);
+        }   
+    public static void main(String args[]){
+        int arr[]= {2,3,-4,5,-1,2,-3,4,-2,6,-5,2};
+          //kadane(arr);
+         kadaneModified(arr);
+
+    }
+}
