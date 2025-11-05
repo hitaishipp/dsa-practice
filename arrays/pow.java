01528c22d0015c95926284b01fd1a0e928bafa01
hitaishipp <hitaishipandey01@gmail.com>
2025-11-05 17:12:09 +0530

    calculation of power

---
+++ arrays/pow.java
@@ -0,0 +1,22 @@
+public class pow{
+    public static double myPow(double x, int n) {
+        double ans=1.0;
+        long nn=n;
+        if(nn<0) nn=-1*nn;
+        while(nn>0){
+        if(nn%2==1){
+            ans*=x;
+            nn=nn-1;
+        }
+        else{
+            x*=x;
+            nn=nn/2;
+        }
+        }
+        if(n<0) ans=(double)1.0 / (double)ans;
+        return ans;
+    }
+    public static void main(String args[]){
+       System.out.println(myPow(2,-3));
+    }
+}
