edd2d86d7213c1ca1c729bfbab41894fc75fc3d0
hitaishipp <hitaishipandey01@gmail.com>
2025-11-11 17:53:03 +0530

    Calculation of power

---
+++ BitManipulation/Power.java
@@ -0,0 +1,17 @@
+public class power {
+    public static void pow(int num,int pow){
+        int ans=1;
+        while(pow>0){
+            if((pow&1)!=0){
+                ans*=num;
+            }
+            num*=num;
+            pow=pow>>1;
+            
+        }
+        System.out.print(ans);
+    }
+    public static void main(String args[]){
+        pow(3,15);
+    }
+}
