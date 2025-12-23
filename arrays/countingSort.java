a3199bd6725916495d07b14525ff20f8bd926d29
hitaishipp <hitaishipandey01@gmail.com>
2025-12-24 04:52:02 +0530

    if mistake corrected

--- arrays/countingSort.java
+++ arrays/countingSort.java
@@ -12,7 +12,7 @@ public class countingSort {
         int j=0;
         for(int i=0;i<max+1;i++){
             
-            if(count[i]==1){
+            while(count[i]>0){
                arr[j]=i;
                j++;
                count[i]--;
@@ -21,7 +21,7 @@ public class countingSort {
         return arr;
     }
     public static void main(String args[]){
-        int arr[]= {1,5,4,2,7,9};
+        int arr[]= {1,5,4,2,7,9,5};
         sort(arr);
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
