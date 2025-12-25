d793b02074d8839dfbf0513e7693e3450bbbacb8
hitaishipp <hitaishipandey01@gmail.com>
2025-12-25 15:52:09 +0530

    binary without consecutive one with thw help of string builder

--- strings/BinaryWithoutConsecutiveOne.java
+++ strings/BinaryWithoutConsecutiveOne.java
@@ -9,6 +9,24 @@ public class BinaryWithoutConsecutiveOne {
             convert(num-1,1,str+"1");
         }
     }
+    public static void noConsOne(int num, int lastbit, StringBuilder s) {//with stringbuilder
+        if (num == 0) {
+            System.out.println(s);
+            return;
+        }
+
+        // Add '0'
+        s.append('0');
+        noConsOne(num - 1, 0, s);
+        s.deleteCharAt(s.length() - 1); // backtrack
+
+        // Add '1' only if last bit was 0
+        if (lastbit == 0) {
+            s.append('1');
+            noConsOne(num - 1, 1, s);
+            s.deleteCharAt(s.length() - 1); // backtrack
+        }
+    }
     public static void main(String args[]){
         int num = 4;
         convert(num,0,"");
