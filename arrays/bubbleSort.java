5183db47793cff34c1d5483c4dfc2f106b0f9bd2
hitaishipp <hitaishipandey01@gmail.com>
2026-01-13 14:02:23 +0530

    optimised bubble sort

--- arrays/bubbleSort.java
+++ arrays/bubbleSort.java
@@ -1,12 +1,17 @@
 public class bubbleSort {
     public static int[] sort(int arr[]){
         for(int i=0;i<arr.length;i++){
+            int flag=0;
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                    int t=arr[j];
                    arr[j]=arr[j+1];
                    arr[j+1]=t;
+                   flag=1;
                 }
+            }
+            if(flag==0){
+                break;
             }
         }
         return arr;
