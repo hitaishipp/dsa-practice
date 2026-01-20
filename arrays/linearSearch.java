0e00be5f69cfe6857415a724fdfea3f0af669463
hitaishipp <hitaishipandey01@gmail.com>
2026-01-20 11:11:00 +0530

    Correct code

--- arrays/linearSearch.java
+++ arrays/linearSearch.java
@@ -1,6 +1,6 @@
 public class linearSearch {
     // Linear search method
-    public static int linearSearch(int[] arr, int target) {
+    public static int lnrsrch(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i; // return index if found
@@ -12,7 +12,7 @@ public class linearSearch {
         int[] arr = {10, 25, 30, 45, 50};
         int target = 30;
 
-        int result = linearSearch(arr, target);
+        int result = lnrsrch(arr, target);
 
         if (result == -1) {
             System.out.println("Element not found in the array.");
