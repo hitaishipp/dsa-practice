cea53463e09b71f218dccd84158cc05c13da5e61
hitaishipp <hitaishipandey01@gmail.com>
2026-02-09 08:41:32 +0530

    removing nodes from end

--- LinkedList/RemoveNthNodeFromEnd.java
+++ LinkedList/RemoveNthNodeFromEnd.java
@@ -1,5 +1,34 @@
 package LinkedList;
 
 public class RemoveNthNodeFromEnd {
-    
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data= data;
+            this.next = null;
+        }
+        
+    }
+    public static Node head;
+
+    public static void search(int pos){
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node fast = dummy;
+        Node slow = dummy;
+        for(int i=0;i<pos && fast!=null;i++){
+            fast=fast.next;
+        }
+        if(fast==null){
+            return ;
+        }
+        while(fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next = slow.next.next;
+        head = dummy.next;
+    }
 }
