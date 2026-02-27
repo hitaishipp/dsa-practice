5395a2a4d429cd7a44a7049f4419f4223731ac21
hitaishipp <hitaishipandey01@gmail.com>
2026-02-27 19:57:24 +0530

    corrected some lines in the code for finding power

--- arrays/pow.java
+++ arrays/pow.java
@@ -4,19 +4,18 @@ public class pow{
         long nn=n;
         if(nn<0) nn=-1*nn;
         while(nn>0){
-        if(nn%2==1){
-            ans*=x;
-            nn=nn-1;
-        }
-        else{
-            x*=x;
-            nn=nn/2;
-        }
+            if(nn%2==1){
+                ans*=x;
+            }
+            
+                x*=x;
+                nn=nn/2;
+            
         }
         if(n<0) ans=(double)1.0 / (double)ans;
         return ans;
     }
     public static void main(String args[]){
-       System.out.println(myPow(2,-3));
+       System.out.println(myPow(3,3));
     }
 }
